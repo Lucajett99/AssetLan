ff56349bcfdec5d0254c28e9b0eec8a8875283b8
MarcoTomasone <tomasone35@gmail.com>
2022-03-22 23:14:47 +0100

    Started Lexical Analysis

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import gen.AssetLanLexer;
+import gen.AssetLanParser;
+import gen.AssetLanVisitor;
+import org.antlr.v4.misc.Graph;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.Interval;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+        String fileName = "src/codeExamples/example1.assetlan";
+        CharStream charStreams = CharStreams.fromFileName(fileName);
+        AssetLanLexer assetLanLexer = new AssetLanLexer(charStreams);
+        CommonTokenStream tokens = new CommonTokenStream(assetLanLexer);
+        AssetLanParser parser = new AssetLanParser(tokens);
+        ParseTree tree = parser.program();
+        //Start Lexical Analysis
+        if(assetLanLexer.lexicalErrors > 0) {
+            System.out.println("Houston Abbiamo " + assetLanLexer.lexicalErrors + " problemi");
+        }
+
+
     }
 }
